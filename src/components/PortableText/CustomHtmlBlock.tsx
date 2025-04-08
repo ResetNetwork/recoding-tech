2fc2e72904965f40ab2eeede8ed3297ccdc5a786
rich.jdev00 <rich.jdev00@gmail.com>
2025-04-09 01:25:09 +0400

    Fix iframe width for correct display on mobile devices

--- src/components/PortableText/CustomHtmlBlock.tsx
+++ src/components/PortableText/CustomHtmlBlock.tsx
@@ -1,10 +1,20 @@
 /* eslint-disable */
 
 import React from "react";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  customDiv: {
+    maxWidth: "100%",
+    "& iframe": {
+      maxWidth: "100%",
+    }
+  },
+}));
 
 export const CustomHtmlBlock = ({ value, children }) => {
-
+  const classes = useStyles();
   const { code } = value;
 
-  return <div dangerouslySetInnerHTML={{__html: code}}></div>;
+  return <div className={classes.customDiv} dangerouslySetInnerHTML={{__html: code}}></div>;
 };
