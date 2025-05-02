69f118e19379836d5718d35e050634ccbb381e69
rich.jdev00 <rich.jdev00@gmail.com>
2025-05-02 22:50:27 +0400

    Update Recent styles

--- src/components/Homepage/FeaturedHero.js
+++ src/components/Homepage/FeaturedHero.js
@@ -50,12 +50,9 @@ const useStyles = makeStyles((theme) => ({
         transition: "left 250ms, top 250ms",
       },
     },
-  },
-  featured: {
     height: 370,
-    position: "relative",
   },
-  featuredContent: {
+  featuredCard: {
     bottom: 0,
     paddingRight: 20,
     position: "absolute",
@@ -75,71 +72,69 @@ function FeaturedHero({ article }) {
     <Container className={classes.container}>
       <Box my={4} mb={10}>
         {article ? (
-          <>
-            <Box mb={10}>
-              <Card
-                variant="outlined"
-                className={`${classes.box} ${classes.featured}`}
-                sx={{
-                  backgroundImage: article.featuredImage
-                    ? `url(${urlFor(article.featuredImage).url()})`
-                    : "",
-                  backgroundRepeat: "no-repeat",
-                  backgroundSize: "100%",
-                  "&:after": {
-                    background:
-                      "linear-gradient(180deg,rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.4) 50%, rgba(0, 0, 0, 0.9) 100%)",
-                    bottom: 0,
-                    content: "''",
-                    height: "100%",
-                    left: 0,
-                    position: "absolute",
-                    right: 0,
-                    top: 0,
-                    width: "100%",
-                    zIndex: 1,
-                  },
+          <Box mb={10}>
+            <Card
+              variant="outlined"
+              className={classes.box}
+              sx={{
+                backgroundImage: article.featuredImage
+                  ? `url(${urlFor(article.featuredImage).url()})`
+                  : "",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "100%",
+                "&:after": {
+                  background:
+                    "linear-gradient(180deg,rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.4) 50%, rgba(0, 0, 0, 0.9) 100%)",
+                  bottom: 0,
+                  content: "''",
+                  height: "100%",
+                  left: 0,
+                  position: "absolute",
+                  right: 0,
+                  top: 0,
+                  width: "100%",
+                  zIndex: 1,
+                },
+              }}
+            >
+              <CardActionArea
+                onClick={(e) => {
+                  e.preventDefault();
+                  articleClick(article.slug.current);
                 }}
+                className={classes.featuredCard}
               >
-                <CardActionArea
-                  onClick={(e) => {
-                    e.preventDefault();
-                    articleClick(article.slug.current);
-                  }}
-                  className={classes.featuredContent}
-                >
-                  <CardContent>
-                    {article.badge && <Badge badge={article.badge} />}
-                    <Typography
-                      gutterBottom
-                      component="div"
-                      variant="h2_article"
-                      color={article.featuredImage ? "#FFF" : "#000"}
-                    >
-                      {titleCase(article.title)}
-                    </Typography>
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      sx={{
-                        color: "#a7a7a7",
-                        fontSize: 14,
-                        fontWeight: 400,
-                        marginTop: "8px",
-                        textTransform: "uppercase",
-                        lineHeight: 1.5,
-                        display: "inline-block",
-                      }}
-                    >
-                      {DateTime.fromISO(article.date)
-                        .setLocale("en-us")
-                        .toLocaleString(DateTime.DATE_FULL)}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Box>
-          </>
+                <CardContent>
+                  {article.badge && <Badge badge={article.badge} />}
+                  <Typography
+                    gutterBottom
+                    component="div"
+                    variant="h2_article"
+                    color={article.featuredImage ? "#FFF" : "#000"}
+                  >
+                    {titleCase(article.title)}
+                  </Typography>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    sx={{
+                      color: "#a7a7a7",
+                      fontSize: 14,
+                      fontWeight: 400,
+                      marginTop: "8px",
+                      textTransform: "uppercase",
+                      lineHeight: 1.5,
+                      display: "inline-block",
+                    }}
+                  >
+                    {DateTime.fromISO(article.date)
+                      .setLocale("en-us")
+                      .toLocaleString(DateTime.DATE_FULL)}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Box>
         ) : null}
       </Box>
     </Container>
